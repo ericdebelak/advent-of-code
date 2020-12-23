113a65c3359b9d2f4f990835ae70b651e0c384d0
Eric Debelak <eric.debelak@call2test.com>
2020-12-23 15:52:28 -0700

    Add task 2

--- 2020/23/tasks.py
+++ 2020/23/tasks.py
@@ -34,3 +34,58 @@ def task_one(cups):
     # remove 1
     cups.popleft()
     return "".join([str(cup) for cup in cups])
+
+
+def test_task_two():
+    assert task_two('389125467') == 149245887792
+    assert task_two('789465123') == 2000455861
+
+
+def task_two(cups):
+    initial_cups = [int(x) for x in cups]
+    # key is cup, value is next cup
+    cups = {}
+    for index, cup in enumerate(initial_cups):
+        if index < len(initial_cups) - 1:
+            cups[cup] = initial_cups[index + 1]
+        else:
+            # this will be the first value in the numeric order numbers
+            cups[cup] = 10
+    # set 10 - 999999
+    for i in range(len(initial_cups) + 1, 1000000):
+        cups[i] = i + 1
+
+    # set our last cup to have the first cup as next
+    cups[1000000] = initial_cups[0]
+
+    current = initial_cups[0]
+
+    for _ in range(10000000):
+        # pick up our next three cups from current
+        first = cups[current]
+        second = cups[first]
+        third = cups[second]
+
+        # reset the next cup on current to be the one after the third picked up cup
+        cups[current] = cups[third]
+
+        picked_up = [first, second, third]
+        # get the destination
+        for i in range(1, current + 1):
+            if current - i == 0:
+                destination = max(cups)
+                break
+            elif current - i not in picked_up:
+                destination = current - i
+                break
+        # the cup after our destination now is after our third picked up cup
+        cups[third] = cups[destination]
+        # the first and second cups have the same cups after them
+        # the destination's next cup is now the first cup picked up
+        cups[destination] = first
+        current = cups[current]
+
+    # our two cups with stars are the ones after 1
+    first_cup_with_star = cups[1]
+    second_cup_with_star = cups[first_cup_with_star]
+    return first_cup_with_star * second_cup_with_star
